5a3e5b01b0f68c4ffe372044a1dfb1a3f7e157a2
gaozhihan <gaozhihan@users.noreply.github.com>
2022-11-02 01:57:06 +0800

    feat: test_earthnet

--- tests/unittests/test_pretrained_checkpoints.py
+++ tests/unittests/test_pretrained_checkpoints.py
@@ -3,13 +3,16 @@ from omegaconf import OmegaConf
 import pytest
 import numpy as np
 import torch
+from torch.nn import functional as F
 import torchmetrics
+from einops import rearrange
 from earthformer.config import cfg
 from earthformer.utils.checkpoint import s3_download_pretrained_ckpt
 from earthformer.utils.layout import layout_to_in_out_slice
 from earthformer.cuboid_transformer.cuboid_transformer import CuboidTransformerModel
 from earthformer.cuboid_transformer.cuboid_transformer_unet_dec import CuboidTransformerAuxModel
 from earthformer.datasets.sevir.sevir_torch_wrap import SEVIRLightningDataModule
+from earthformer.datasets.earthnet.earthnet_dataloader import get_EarthNet2021_dataloaders
 
 
 def config_cuboid_transformer(cfg, model_type="CuboidTransformerModel"):
@@ -80,9 +83,9 @@ def test_sevir():
         num_workers=8, )
     dm.prepare_data()
     dm.setup()
-    in_slice, out_slice = layout_to_in_out_slice(layout=layout_cfg["layout"],
-                                                 in_len=layout_cfg["in_len"],
-                                                 out_len=layout_cfg["out_len"])
+    in_slice, out_slice = layout_to_in_out_slice(layout=layout_cfg.layout,
+                                                 in_len=layout_cfg.in_len,
+                                                 out_len=layout_cfg.out_len)
     test_mse_metrics = torchmetrics.MeanSquaredError().to(device)
     test_mae_metrics = torchmetrics.MeanAbsoluteError().to(device)
 
@@ -93,6 +96,102 @@ def test_sevir():
         y_hat = model(x)
         test_mse_metrics(y_hat, y)
         test_mae_metrics(y_hat, y)
+        break
+
+    test_mse = test_mse_metrics.compute()
+    test_mae = test_mae_metrics.compute()
+    assert test_mse < 3E-5
+    assert test_mae < 3E-3
+
+def test_earthnet():
+    micro_batch_size = 1
+    pretrained_ckpt_name = "earthformer_earthnet2021.pt"
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    # Load pretrained model
+    pretrained_cfg_path = os.path.join(cfg.root_dir, "scripts", "cuboid_transformer", "earthnet_w_meso", "cfg_pretrained.yaml")
+    pretrained_cfg = OmegaConf.load(open(pretrained_cfg_path, "r"))
+    model = config_cuboid_transformer(
+        cfg=pretrained_cfg,
+        model_type="CuboidTransformerAuxModel").to(device)
+
+    if not os.path.exists(os.path.join(cfg.pretrained_checkpoints_dir, pretrained_ckpt_name)):
+        s3_download_pretrained_ckpt(ckpt_name=pretrained_ckpt_name,
+                                    save_dir=cfg.pretrained_checkpoints_dir,
+                                    exist_ok=False)
+    state_dict = torch.load(os.path.join(cfg.pretrained_checkpoints_dir, pretrained_ckpt_name),
+                            map_location=device)
+    missing_keys, unexpected_keys = model.load_state_dict(state_dict=state_dict, strict=False)
+    assert len(missing_keys) == 0, f"missing_keys {missing_keys} when loading pretrained state_dict."
+    assert len(unexpected_keys) == 0, f"missing_keys {unexpected_keys} when loading pretrained state_dict."
+    # Test on SEVIR test
+    dataset_cfg = OmegaConf.to_object(pretrained_cfg.dataset)
+    layout_cfg = pretrained_cfg.layout
+    data_channels = pretrained_cfg.model.data_channels
+    test_dataloader = get_EarthNet2021_dataloaders(
+        dataloader_return_mode=dataset_cfg["return_mode"],
+        data_aug_mode=dataset_cfg["data_aug_mode"],
+        data_aug_cfg=dataset_cfg["data_aug_cfg"],
+        test_subset_name=dataset_cfg["test_subset_name"],
+        val_ratio=dataset_cfg["val_ratio"],
+        train_val_split_seed=dataset_cfg["train_val_split_seed"],
+        layout=dataset_cfg["layout"],
+        static_layout=dataset_cfg["static_layout"],
+        highresstatic_expand_t=dataset_cfg["highresstatic_expand_t"],
+        mesostatic_expand_t=dataset_cfg["mesostatic_expand_t"],
+        meso_crop=dataset_cfg["meso_crop"],
+        fp16=dataset_cfg["fp16"],
+        batch_size=micro_batch_size,
+        num_workers=1,
+    )["test_dataloader"][0]
+    in_slice, out_slice = layout_to_in_out_slice(layout=layout_cfg.layout,
+                                                 in_len=layout_cfg.in_len,
+                                                 out_len=layout_cfg.out_len)
+    test_mse_metrics = torchmetrics.MeanSquaredError().to(device)
+    test_mae_metrics = torchmetrics.MeanAbsoluteError().to(device)
+
+    for batch in test_dataloader:
+        highresdynamic = batch["highresdynamic"]
+        seq = highresdynamic[..., :data_channels]
+        # mask from dataloader: 1 for mask and 0 for non-masked
+        mask = highresdynamic[..., data_channels: data_channels + 1][out_slice]
+
+        in_seq = seq[in_slice]
+        target_seq = seq[out_slice]
+
+        # process aux data
+        highresstatic = batch["highresstatic"]  # (b c h w)
+        mesodynamic = batch["mesodynamic"]  # (b t h w c)
+        mesostatic = batch["mesostatic"]  # (b c h w)
+
+        mesodynamic_interp = rearrange(mesodynamic,
+                                       "b t h w c -> b c t h w")
+        mesodynamic_interp = F.interpolate(mesodynamic_interp,
+                                           size=(layout_cfg.in_len + layout_cfg.out_len,
+                                                 layout_cfg.img_height,
+                                                 layout_cfg.img_width),
+                                           mode="nearest")
+        highresstatic_interp = rearrange(highresstatic,
+                                         "b c h w -> b c 1 h w")
+        highresstatic_interp = F.interpolate(highresstatic_interp,
+                                             size=(layout_cfg.in_len + layout_cfg.out_len,
+                                                   layout_cfg.img_height,
+                                                   layout_cfg.img_width),
+                                             mode="nearest")
+        mesostatic_interp = rearrange(mesostatic,
+                                      "b c h w -> b c 1 h w")
+        mesostatic_interp = F.interpolate(mesostatic_interp,
+                                          size=(layout_cfg.in_len + layout_cfg.out_len,
+                                                layout_cfg.img_height,
+                                                layout_cfg.img_width),
+                                          mode="nearest")
+        aux_data = torch.cat((highresstatic_interp, mesodynamic_interp, mesostatic_interp),
+                             dim=1)
+        aux_data = rearrange(aux_data,
+                             "b c t h w -> b t h w c")
+
+        pred_seq = model(in_seq, aux_data[in_slice], aux_data[out_slice])
+        test_mse_metrics(pred_seq * (1 - mask), target_seq * (1 - mask))
+        test_mae_metrics(pred_seq * (1 - mask), target_seq * (1 - mask))
         break
 
     test_mse = test_mse_metrics.compute()
