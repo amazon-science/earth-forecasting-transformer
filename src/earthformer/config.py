62ffb0ecaf2819c830c29dffedf1886feb526fcd
Xingjian Shi <xshiab@connect.ust.hk>
2022-11-07 12:22:38 -0800

    Update config.py

--- src/earthformer/config.py
+++ src/earthformer/config.py
@@ -7,6 +7,7 @@ print("_CURR_DIR=", _CURR_DIR)
 
 cfg = OmegaConf.create()
 cfg.root_dir = os.path.abspath(os.path.realpath(os.path.join(_CURR_DIR, "..", "..")))
+print("cfg.root_dir=", cfg.root_dir)
 cfg.datasets_dir = os.path.join(cfg.root_dir, "datasets")  # default directory for loading datasets
 cfg.pretrained_checkpoints_dir = os.path.join(cfg.root_dir, "pretrained_checkpoints")  # default directory for saving and loading pretrained checkpoints
 cfg.exps_dir = os.path.join(cfg.root_dir, "experiments")  # default directory for saving experiment results
