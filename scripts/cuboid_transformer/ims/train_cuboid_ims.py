4a29080c68a16129141b9a4a61e23cfed274097f
leahandofir <leahandofir@gmail.com>
2023-05-25 19:14:21 +0300

    tip tipa seder babalagan

--- scripts/cuboid_transformer/ims/train_cuboid_ims.py
+++ scripts/cuboid_transformer/ims/train_cuboid_ims.py
@@ -15,18 +15,16 @@ class CuboidIMSModule(pl.LightningModule):
         super().__init__()
 
         self.model = CuboidTransformerModel(
-            input_shape=model_cfg["input_shape"],
-            target_shape=model_cfg["target_shape"],
-            base_units=model_cfg["base_units"],
-            block_units=model_cfg["block_units"],
-            scale_alpha=model_cfg["scale_alpha"],
+            
+            # --------------------- network arch/size --------------------- #
+
+            # general encoder/decoder configs
             enc_depth=model_cfg["enc_depth"],
             dec_depth=model_cfg["dec_depth"],
             enc_use_inter_ffn=model_cfg["enc_use_inter_ffn"],
             dec_use_inter_ffn=model_cfg["dec_use_inter_ffn"],
-            dec_hierarchical_pos_embed=model_cfg["dec_hierarchical_pos_embed"],
-            downsample=model_cfg["downsample"],
-            downsample_type=model_cfg["downsample_type"],
+            dec_hierarchical_pos_embed=model_cfg["dec_hierarchical_pos_embed"], 
+            # attention related
             enc_attn_patterns=enc_attn_patterns,
             dec_self_attn_patterns=dec_self_attn_patterns,
             dec_cross_attn_patterns=dec_cross_attn_patterns,
@@ -34,13 +32,25 @@ class CuboidIMSModule(pl.LightningModule):
             dec_use_first_self_attn=model_cfg["dec_use_first_self_attn"],
             num_heads=model_cfg["num_heads"],
             attn_drop=model_cfg["attn_drop"],
+            # in/out shape
+            input_shape=model_cfg["input_shape"],
+            target_shape=model_cfg["target_shape"],
+            # TODO: what are these?
+            base_units=model_cfg["base_units"],
+            block_units=model_cfg["block_units"], # this is null in cfg_ims.yaml
+            scale_alpha=model_cfg["scale_alpha"], # not necessary ? 
+            # stuff that hopefully helps (dropout, up/downsampling, good activations, normalization)
             proj_drop=model_cfg["proj_drop"],
             ffn_drop=model_cfg["ffn_drop"],
             upsample_type=model_cfg["upsample_type"],
+            downsample=model_cfg["downsample"],  
+            downsample_type=model_cfg["downsample_type"],
             ffn_activation=model_cfg["ffn_activation"],
             gated_ffn=model_cfg["gated_ffn"],
             norm_layer=model_cfg["norm_layer"],
-            # global vectors
+
+            # ----------------------- global vectors ---------------------- #
+
             num_global_vectors=model_cfg["num_global_vectors"],
             use_dec_self_global=model_cfg["use_dec_self_global"],
             dec_self_update_global=model_cfg["dec_self_update_global"],
@@ -49,22 +59,27 @@ class CuboidIMSModule(pl.LightningModule):
             use_global_self_attn=model_cfg["use_global_self_attn"],
             separate_global_qkv=model_cfg["separate_global_qkv"],
             global_dim_ratio=model_cfg["global_dim_ratio"],
-            # initial_downsample
+
+            # --------------------- initial_downsample -------------------- #
+
             initial_downsample_type=model_cfg["initial_downsample_type"],
             initial_downsample_activation=model_cfg["initial_downsample_activation"],
-            # initial_downsample_type=="stack_conv"
+            # these are relevant when (initial_downsample_type == "stack_conv")
             initial_downsample_stack_conv_num_layers=model_cfg["initial_downsample_stack_conv_num_layers"],
             initial_downsample_stack_conv_dim_list=model_cfg["initial_downsample_stack_conv_dim_list"],
             initial_downsample_stack_conv_downscale_list=model_cfg["initial_downsample_stack_conv_downscale_list"],
             initial_downsample_stack_conv_num_conv_list=model_cfg["initial_downsample_stack_conv_num_conv_list"],
-            # misc
+            
+            # ----------------------------- misc -------------------------- #
+
             padding_type=model_cfg["padding_type"],
             z_init_method=model_cfg["z_init_method"],
             checkpoint_level=model_cfg["checkpoint_level"],
             pos_embed_type=model_cfg["pos_embed_type"],
             use_relative_pos=model_cfg["use_relative_pos"],
             self_attn_use_final_proj=model_cfg["self_attn_use_final_proj"],
-            # initialization
+
+            # ----------------------- initialization ---------------------- #
             attn_linear_init_mode=model_cfg["attn_linear_init_mode"],
             ffn_linear_init_mode=model_cfg["ffn_linear_init_mode"],
             conv_init_mode=model_cfg["conv_init_mode"],
@@ -96,18 +111,26 @@ class CuboidIMSModule(pl.LightningModule):
         optimizer = AdamW(self.parameters()) # TODO: give params
         return optimizer
 
+
+
 def main():
     # model
     l_model = CuboidIMSModule()
 
     # data
-    dm = IMSLightningDataModule(start_date=datetime(*(2023,1,1)), train_val_split_date=datetime(*(2023,1,2)), train_test_split_date=datetime(*(2023,1,3)), end_date=datetime(*(2023,1,9)), seq_len=20)
+    dm = IMSLightningDataModule(start_date=datetime(*(2023,1,1)), 
+                                train_val_split_date=datetime(*(2023,1,2)), 
+                                train_test_split_date=datetime(*(2023,1,3)), 
+                                end_date=datetime(*(2023,1,9)), 
+                                seq_len=20)
     dm.prepare_data()
     dm.setup()
 
     # train model
     trainer = pl.Trainer(max_epochs=1)
     trainer.fit(model=l_model, train_dataloaders=dm.train_dataloader())
+
+
 
 if __name__ == "__main__":
     main()
